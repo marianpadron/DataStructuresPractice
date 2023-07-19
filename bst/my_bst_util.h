90df075f99c1bf6fa9a2f9ac1dcc634f336fc542
Marian Padron <139380027+marianpadron@users.noreply.github.com>
2023-07-19 10:18:53 -0400

    Update my_bst_util.h

---
+++ bst/my_bst_util.h
@@ -0,0 +1,17 @@
+/**
+ * Some standard utility functions for binary search trees.
+ * @author Marian Padron
+ * Semester Spring 2023
+ */
+#ifndef MYBSTUTIL_H
+#define MYBSTUTIL_H
+
+#include <stdlib.h>
+#include "my_bst.h"
+
+int sum(BST* tree);
+int min(BST* tree);
+int max(BST* tree);
+
+
+#endif
